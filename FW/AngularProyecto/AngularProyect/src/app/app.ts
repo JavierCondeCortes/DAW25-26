8c3350d3932ee520c019c39951aecb9d6c00ee11
JavierCondeCortes <jccfmys@gmail.com>
2025-09-25 14:55:30 +0200

    creacion de nuevo proyecto dados

--- FW/AngularProyecto/AngularProyect/src/app/app.ts
+++ FW/AngularProyecto/AngularProyect/src/app/app.ts
@@ -8,5 +8,51 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularProyect');
+  nombre = 'Juan Francisco';
+  apellidos = "Conde Augusto";
+  sueldos=[2300,3440,1800];
+  activo = true;
+  sitio='https://google.com';
+
+  edad = 40;
+
+  articulos = [{
+    codigo: 1,
+    descripcion: 'naranjas',
+    precio: 540
+  }, {
+    codigo: 2,
+    descripcion: 'manzanas',
+    precio: 900
+  }, {
+    codigo: 3,
+    descripcion: 'peras',
+    precio: 490
+  }];
+
+  generarNumero() {
+    return Math.floor(Math.random() * 3) + 1;
+  }
+
+    contador = 1;
+
+  incrementar() {
+    this.contador++;
+  }
+
+  decrementar() {
+    this.contador--;
+  }
+
+nombre2 = ''
+
+  fijarNombre1() {
+    this.nombre2 = 'Juan';
+  }
+
+  fijarNombre2() {
+    this.nombre2 = 'Ana';
+  }
+
 }
+
---
+++ FW/AngularProyecto/Poryecto1/src/app/components/dado/dado.ts
@@ -0,0 +1,12 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  imports: [],
+  templateUrl: './dado.html',
+  styleUrl: './dado.css'
+})
+export default class Dado {
+  @Input() valor: string= "";
+
+}
