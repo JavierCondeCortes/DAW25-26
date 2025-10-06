5bae885ff3628af53121469545a6da7accebcacc
MiNeClaSeRo <jccfmys@gmail.com>
2025-10-06 23:13:44 +0200

    realización de tareas js de entorno cliente

---
+++ DWEC/Practicas_JS/Practicas_UT2/HojaTareasEjercicios/tarea22case.js
@@ -0,0 +1,15 @@
+let valor=2;
+
+switch(valor){
+    case 1:
+        console.log(valor);
+        break;
+    case 2:
+        console.log(valor);
+        break;
+    case 3:
+        console.log(valor);
+        break;
+    default:
+        console.log("resultado resto")
+}
