e561b6d5f45e59d7c099ba26e2be504eb070b17c
JavierCondeCortes <jccfmys@gmail.com>
2025-10-14 12:27:25 +0200

    php provincias

---
+++ DWES/Practicas_PHP/Practicas_UT5/tarea2ProvinciasLocalidades2.php
@@ -0,0 +1,41 @@
+<?php
+//CONEXION A LA BASE DE DATOS
+
+try {
+
+    $conn = new PDO('mysql:host=localhost;dbname=geografia', 'root', '');
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    echo "<p>error</p> " . $e->getMessage();
+}
+
+//CONSULTA DE EXISTENCIA DE CIUDAD
+
+$sql = "SELECT nombre 
+        FROM provincias
+        WHERE nombre = :nombre";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':nombre', ($_GET["ciudad"]));
+$stmt->execute();
+
+$coincidencia = $stmt->fetch(PDO::FETCH_ASSOC); //COMPARACION DE QUE EXISTA COINCIDA CON LA CONSULTA
+?>
+
+
+
+<?php if(isset($_GET["buscar"])) : ?>
+    <?php if($coincidencia) :?> <!--evitar que meta un dato inexsistente-->
+        <h1>Buscando información sobre <?=($_GET["ciudad"])?></h1>
+    <?php else :?>
+        <?php
+            header("Location: tarea2ProvinciasLocalidades.html"); 
+            exit;
+        ?>
+        
+        
+    <?php endif ;?>
+<?php else :?>
+    
+<?php endif ;?>
