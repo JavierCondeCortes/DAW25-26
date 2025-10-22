b74db5f88c965ad1b3c4e20a1ff471a6e168b6f2
JavierCondeCortes <jccfmys@gmail.com>
2025-10-22 15:00:03 +0200

    ya ta

--- DWEC/PracticasExamen/Conde_Cortes_javier_Practica2.js
+++ DWEC/PracticasExamen/Conde_Cortes_javier_Practica2.js
@@ -10,9 +10,10 @@ console.log(f4SinEspacios);
 let f5LogintudMedia = FrasePrueba.split(" ")
     .map((palabra)  => (palabra))
     .filter((palabra) => palabra.length)
-    .reduce((acc,palabraFiltrada) => acc+palabraFiltrada.length,0);
+    .reduce((acc,palabraFiltrada) => acc+palabraFiltrada.length,0) / FrasePrueba.split(" ").length;
 
 console.log(f5LogintudMedia);
 
-// let f6ArrayLongitudPalabra = FrasePrueba.find(palabra => palabra == "a")
-//     .reduce((acc,palabraFiltrada) => acc+palabraFiltrada.length,0);
+let f6ArrayLongitudPalabra = FrasePrueba.split(" ")
+    .filter(palabra => palabra.includes("a"))
+    .reduce((acc,palabraFiltrada) => acc+palabraFiltrada.length,0);
