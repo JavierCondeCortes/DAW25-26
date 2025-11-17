19625ee4c0e64bd4fea6dcd882a228c381b7f051
JavierCondeCortes <jccfmys@gmail.com>
2025-11-17 14:03:00 +0100

    cosas

---
+++ FW/examen/tienda-Javier-CondeCortes/src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListadoProductos } from '../listado-productos/listado-productos';
+import { DetalleProducto } from '../detalle-producto/detalle-producto';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,ListadoProductos, DetalleProducto],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('tienda-Javier-CondeCortes');
+
+  mensaje = "";
+
+  actualizado(pulso:string){
+    this.mensaje = "pulsado";
+  }
+
+  oferta(oferta: string){
+    this.mensaje = "oferta aplicada";
+  }
+}
+
