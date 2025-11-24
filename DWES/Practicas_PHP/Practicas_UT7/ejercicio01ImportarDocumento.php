a8c09e4bb4e6a08a9fd017bf72f515f3e932e4c1
JavierCondeCortes <jccfmys@gmail.com>
2025-11-24 14:58:18 +0100

    php ProyectoVideoClub 24/11/25

---
+++ DWES/Practicas_PHP/Practicas_UT7/ejercicio01ImportarDocumento.php
@@ -0,0 +1,10 @@
+<?php 
+include_once("ejercicio01Importar/documentoExportar.php");
+
+use const ejercicio01Importar\IVA;
+use ejercicio01Importar\Producto;
+
+echo IVA;
+$p1 = new Producto();
+
+?>
