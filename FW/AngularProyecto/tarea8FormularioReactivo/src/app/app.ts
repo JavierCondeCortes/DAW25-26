a8b534d693daef93fd1ce7c54e93b8aeefaf0f22
JavierCondeCortes <jccfmys@gmail.com>
2025-11-24 13:04:08 +0100

    FW 24/11/25

---
+++ FW/AngularProyecto/tarea8FormularioReactivo/src/app/app.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ReactiveFormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  actividad = new FormControl();
+  lista: string[];
+
+  constructor(){
+    this.lista = [];
+    let datos = localStorage.getItem("actividades");
+    if(datos!= null){
+      let array = JSON.parse(datos);
+      if(array != null){
+        for(let actividad of array){
+          this.lista.push(actividad);
+        }
+      }
+    }
+  }
+  agregar(){
+    this.lista.push(this.actividad.value);
+    localStorage.setItem('actividades', JSON.stringify(this.lista)) //persistencia de datos (no se borran)
+    this.actividad.setValue('');
+  }
+  borrar(pos:number){
+    this.lista.splice(pos,1);
+    localStorage.clear();
+    localStorage.setItem('actividades', JSON.stringify(this.lista))
+  }
+  borrarTodas(){
+    localStorage.clear();
+    this.lista=[];
+  }
+}
