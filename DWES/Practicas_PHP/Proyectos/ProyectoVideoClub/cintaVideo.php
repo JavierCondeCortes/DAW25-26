c9e02ec4e4ddba58439bca6f34cd89a9207838d2
JavierCondeCortes <jccfmys@gmail.com>
2025-12-02 14:05:30 +0100

    php

--- DWES/Practicas_PHP/Proyectos/ProyectoVideoClub/cintaVideo.php
+++ DWES/Practicas_PHP/Proyectos/ProyectoVideoClub/cintaVideo.php
@@ -1,25 +1,4 @@
-<!-- include_once("soporte.php");
-
-class cintaVideo extends Soporte{
-    public function __construct(
-        int $numero,
-        int $precio,
-        string $titulo,
-        private int $duracion,
-    ){
-        parent::__construct($numero);
-        parent::__construct($precio);
-        parent::__construct($titulo);
-    }
-
-    public function muestraResumen()
-    {
-        return parent::muestraResumen();
-        echo "<p>".$this->duracion."</p>";
-    }
-} -->
-
-<?php 
+<?php
 include_once("Soporte.php");
 
 class CintaVideo extends Soporte {
--- DWES/Practicas_PHP/Proyectos/ProyectoVideoClub/juego.php
+++ DWES/Practicas_PHP/Proyectos/ProyectoVideoClub/juego.php
@@ -1,33 +1,5 @@
-<!-- include_once("soporte.php");
-
-class cintaVideo extends Soporte{
-    public function __construct(
-        int $numero,
-        int $precio,
-        string $titulo,
-        private int $minNumJugadores,
-        private int $maxNumJugadores,
-        public string $consola,
-    ){
-        parent::__construct($numero);
-        parent::__construct($precio);
-        parent::__construct($titulo);
-    }
-
-    public function jugadoresPosibles(){
-        echo "<p>Jugadores Posibles".($this->maxNumJugadores - $this->minNumJugadores)."</p>";
-    }
-
-    public function muestraResumen()
-    {
-        return parent::muestraResumen();
-        echo "<p>".$this->minNumJugadores."</p>";
-        echo "<p>".$this->maxNumJugadores."</p>";
-    }
-} -->
-
-<?php 
-include_once("soporte.php");
+<?php
+include_once("Soporte.php");
 
 class Juego extends Soporte {
     public function __construct(
@@ -37,19 +9,18 @@ class Juego extends Soporte {
         private int $minNumJugadores,
         private int $maxNumJugadores,
         public string $consola
-    ){
+    ) {
         parent::__construct($numero, $precio, $titulo);
     }
 
     public function jugadoresPosibles() {
-        echo "<p>Jugadores posibles: ".($this->maxNumJugadores - $this->minNumJugadores)."</p>";
+        echo "<p>Jugadores posibles: ".$this->minNumJugadores." - ".$this->maxNumJugadores."</p>";
     }
 
     public function muestraResumen() {
         parent::muestraResumen();
-        echo "<p>Jugadores: ".$this->minNumJugadores." - ".$this->maxNumJugadores."</p>";
+        $this->jugadoresPosibles();
         echo "<p>Consola: ".$this->consola."</p>";
     }
 }
 ?>
-
