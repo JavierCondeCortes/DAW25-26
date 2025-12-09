404d1917a2a4cfaad2201b959ad7f036966b2160
JavierCondeCortes <jccfmys@gmail.com>
2025-12-09 12:26:16 +0100

    laravel

---
+++ DWES/Practicas_PHP/FrameworkLarabel/proyecto/resources/views/si.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    {!! 'hola' !!}
+    <p>el nombre es: {{$nombre}}</p>
+    <p>El ID es: {{ $id }}</p>
+    
+</body>
+</html>
