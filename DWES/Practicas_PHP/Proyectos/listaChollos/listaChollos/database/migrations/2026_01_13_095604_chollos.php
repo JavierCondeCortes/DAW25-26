03bf1f86eb784e31aec1e9aebea25e77cda61527
JavierCondeCortes <jccfmys@gmail.com>
2026-01-13 12:20:17 +0100

    servidor

---
+++ DWES/Practicas_PHP/Proyectos/listaChollos/listaChollos/database/migrations/2026_01_13_095604_chollos.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('oferta_chollos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->string('url');
+
+            $table->foreignId('categoria_id')
+                ->constrained('categorias')
+                ->onDelete('cascade');
+
+
+            $table->integer('puntuacion');
+            $table->integer('precio');
+            $table->integer('precio_descuento');
+            $table->boolean('disponible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('oferta_chollos');
+    }
+};
