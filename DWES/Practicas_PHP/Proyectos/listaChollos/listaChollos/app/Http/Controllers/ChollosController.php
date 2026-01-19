8d8a54e0d6e7c4cbd6fb41f3390f9cbf3b57c67f
JavierCondeCortes <jccfmys@gmail.com>
2026-01-19 14:56:13 +0100

    Servidor

---
+++ DWES/Practicas_PHP/Proyectos/listaChollos/listaChollos/app/Http/Controllers/ChollosController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+use App\Models\Chollo;
+
+
+class ChollosController extends Controller
+{
+    public function detalles($id)
+    {
+        $chollo = Chollo::findOrFail($id);
+        return view('chollos.detallesProducto', compact('chollo'));
+    }
+
+    public function nuevoChollo(Request $request){
+        $nuevoChollo = new Chollo;
+            $nuevoChollo -> titulo = $request -> titulo;
+            $nuevoChollo -> descripcion  -> $request -> descripcion;
+            $nuevoChollo -> url ->$request->url;
+            $nuevoChollo -> categoria_id ->$request->categoria_id;
+            $nuevoChollo -> puntuacion ->$request->puntuacion;
+            $nuevoChollo -> precio ->$request->precio;
+            $nuevoChollo -> precio_descuento ->$request->precio_descuento;
+
+            $nuevoChollo -> save();
+
+            return redirect()->route('chollos.index');
+        
+    }
+}
+
---
+++ DWES/Practicas_PHP/Proyectos/listaChollos/listaChollos/resources/views/chollos/detallesProducto.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @extends('chollos/navbar')
+</head>
+<body>
+    @section('content')
+        
+    <h1>{{ $chollo -> titulo }}</h1>
+    <p>{{ $chollo -> descripcion }}</p>
+    <p>precio sin descuento: {{ $chollo -> precio }}€</p>
+    <p>precio con descuento: {{ $chollo -> precio_descuento }}€</p>
+    <p>precio con descuento: {{ $chollo -> categoria->name}}</p>
+
+    <button class=" bg-slate-700 p-2 rounded text-white hover:bg-slate-400 hover:text-black transition-all"> <a href="">editar datos</a></button>
+    <button class=" bg-slate-700 p-2 rounded text-white hover:bg-slate-400 hover:text-black transition-all"> <a href="{{route('inicio')}}">regresar</a></button>
+    
+    @endsection
+</body>
+</html>
