c921258a454f5d42dcea39dd1be242987ac6bd21
JavierCondeCortes <jccfmys@gmail.com>
2026-01-19 12:22:08 +0100

    FW

--- FW/AngularProyecto/practica7CatalogoTienda/src/app/contacto/contacto.ts
+++ FW/AngularProyecto/practica7CatalogoTienda/src/app/contacto/contacto.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
 export class Contacto {
 
+  dato='';
+
+  formulario = new FormGroup({
+    nombre: new FormControl(''),
+    mail: new FormControl(''),
+    mensaje: new FormControl(''),
+  })
+
+  submit(){
+    this.dato = `Nombre= ${this.formulario.value.nombre}
+                  mail= ${this.formulario.value.mail}
+                  mensaje= ${this.formulario.value.mensaje}`;
+  }
 }
+
