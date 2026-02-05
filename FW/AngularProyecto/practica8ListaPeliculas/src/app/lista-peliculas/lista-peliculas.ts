d91edfe3db7e336f7e4612b68913e1952dbb9dfc
JavierCondeCortes <jccfmys@gmail.com>
2026-02-05 14:07:01 +0100

    fw

--- FW/AngularProyecto/practica8ListaPeliculas/src/app/lista-peliculas/lista-peliculas.ts
+++ FW/AngularProyecto/practica8ListaPeliculas/src/app/lista-peliculas/lista-peliculas.ts
@@ -1,16 +1,18 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { PeliculasService } from '../peliculas';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-lista-peliculas',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './lista-peliculas.html',
   styleUrl: './lista-peliculas.css',
 })
 export class ListaPeliculas {
+  peliculas:any;
 
-  constructor(private http: HttpClient){ };
-  retornar(){
-    // return this.http.get();
+  constructor(private peliculasService: PeliculasService){
+    this.peliculasService.retornar()
+    .subscribe(result=> this.peliculas = result);
   }
 }
